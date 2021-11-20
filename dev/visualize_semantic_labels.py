bca25e94dc321057c6a48dad5defe1f024e70d1c
David Russell <david.russell@kitware.com>
2021-11-20 12:52:51 -0500

    Make label visualization more user-friendly

--- dev/visualize_semantic_labels.py
+++ dev/visualize_semantic_labels.py
@@ -2,7 +2,7 @@
 Show the segmentation labels from TartanAir to see if they have 
 Any semantic meaning
 """
-
+import argparse
 from glob import glob
 from pathlib import Path
 
@@ -12,18 +12,40 @@ from skimage import io
 from tqdm import tqdm
 from ubelt.util_path import ensuredir
 
-SEG_MAP = np.loadtxt("dev/seg_rgbs.txt")
+SEG_MAP = "dev/seg_rgbs.txt"
 
 
-def visualize(seg_dir, image_dir, output_dir, seg_map=SEG_MAP):
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image-dir", type=Path)
+    parser.add_argument("--output-dir", type=Path)
+    parser.add_argument("--seg-dir", type=Path)
+    parser.add_argument("--seg-map", default=SEG_MAP, type=Path)
+    args = parser.parse_args()
+    return args
+
+
+def load_png_npy(filename):
+    if filename.suffix == ".npy":
+        return np.load(filename)
+    elif filename.suffix in (".png", ".jpg", ".jpeg"):
+        return io.imread(filename)
+
+
+def visualize(seg_dir, image_dir, output_dir, seg_map_file=SEG_MAP):
+    seg_map = np.loadtxt(seg_map_file)
     ensuredir(output_dir)
-    seg_files = sorted(seg_dir.glob("*.npy"))
-    image_files = sorted(image_dir.glob("*.png"))
+    seg_files = sorted(
+        list(Path(seg_dir).glob("*.npy")) + list(Path(seg_dir).glob("*.png"))
+    )
+    image_files = sorted(Path(image_dir).glob("*.png"))
     if len(seg_files) != len(image_files):
-        raise ValueError("Different length inputs")
+        raise ValueError(
+            f"Different length inputs, {len(seg_files)}, {len(image_files)}"
+        )
 
     for seg_file, image_file in tqdm(zip(seg_files, image_files), total=len(seg_files)):
-        seg = np.load(seg_file)
+        seg = load_png_npy(seg_file)
         img = io.imread(image_file)
         vis_seg = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
 
@@ -35,17 +57,20 @@ def visualize(seg_dir, image_dir, output_dir, seg_map=SEG_MAP):
         io.imsave(savepath, concat)
 
 
-# SEG_DIR = Path("data/P001/seg_left")
-# IMAGE_DIR = Path("data/P001/image_left")
-# OUTPUT_DIR = Path("vis/P001")
-# visualize(SEG_DIR, IMAGE_DIR, OUTPUT_DIR)
+if __name__ == "__main__":
+    args = parse_args()
+    visualize(args.seg_dir, args.image_dir, args.output_dir, args.seg_map)
+    # SEG_DIR = Path("data/P001/seg_left")
+    # IMAGE_DIR = Path("data/P001/image_left")
+    # OUTPUT_DIR = Path("vis/P001")
+    # visualize(SEG_DIR, IMAGE_DIR, OUTPUT_DIR)
 
-SEG_DIR = Path("data/P002/seg_left")
-IMAGE_DIR = Path("data/P002/image_left")
-OUTPUT_DIR = Path("vis/P002")
-visualize(SEG_DIR, IMAGE_DIR, OUTPUT_DIR)
-
-SEG_DIR = Path("data/P006/seg_left")
-IMAGE_DIR = Path("data/P006/image_left")
-OUTPUT_DIR = Path("vis/P006")
-visualize(SEG_DIR, IMAGE_DIR, OUTPUT_DIR)
+    # SEG_DIR = Path("data/P002/seg_left")
+    # IMAGE_DIR = Path("data/P002/image_left")
+    # OUTPUT_DIR = Path("vis/P002")
+    # visualize(SEG_DIR, IMAGE_DIR, OUTPUT_DIR)
+    #
+    # SEG_DIR = Path("data/P006/seg_left")
+    # IMAGE_DIR = Path("data/P006/image_left")
+    # OUTPUT_DIR = Path("vis/P006")
+    # visualize(SEG_DIR, IMAGE_DIR, OUTPUT_DIR)
