96b890b7b330a4df599200cc6b4cafa50f34c249
David Russell <david.russell@kitware.com>
2021-12-07 13:09:25 -0500

    Another script for running inference

--- scripts/test_bisenet.sh
+++ scripts/test_bisenet.sh
@@ -8,11 +8,12 @@ cd /jet/home/russelld/dev/BiSeNet
 source ~/anaconda3/etc/profile.d/conda.sh
 
 conda activate pytorch181
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1
 cfg_file=configs/bisenetv1_synthetic.py
-NGPUS=2
 echo "cuda is available: "
 python -c 'import torch; print(torch.cuda.is_available())'
+echo $CONDA_PREFIX
+
 python tools/demo_video.py --config configs/bisenetv1_synthetic.py \
     --weight-path /ocean/projects/tra190016p/russelld/SafeForestData/models/synthetic/bisenetv1-synthetic-train-2021-11-23-11-54-23/model_final.pth \
     --input /ocean/projects/tra190016p/russelld/SafeForestData/datasets/first_synthetic_delivery_derived/test.mp4 \
