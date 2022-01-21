783f674582176548e83d87d0753bcea8338461df
David Russell <davidrussell327@gmail.com>
2022-01-21 18:18:25 -0500

    Initial successful projection

--- dev/visualize_mesh_and_traj.py
+++ dev/visualize_mesh_and_traj.py
@@ -13,6 +13,12 @@ TRAJECTORY_FILE = Path(
     "data/SafeForestData/datasets/portugal_UAV_12_21/derived/safe_forest_2/slam_outputs/Odom_camera_left/2022-01-14-17-44/odom.txt",
 )
 COLOR_MAP = np.array([[128, 128, 128], [0, 255, 0], [150, 75, 0]])
+K = np.array([[719.4674, 0, 682.85536], [0, 719.4674, 555.98205], [0, 0, 1]])
+IMSIZE = (1384, 1032)
+
+
+def check_points(x, y, imsize):
+    return np.all((x >= 0, x < imsize[0], y >= 0, y < imsize[1]))
 
 
 def read_trajectory(file):
@@ -35,6 +41,26 @@ def read_pointcloud(file):
     return xyzs, labels
 
 
+def project_points(points, colors, loc, rot, K, imsize):
+    """
+    imsize: tuple 
+        (W, H)
+    """
+    points = points - loc
+
+    local_points = np.linalg.inv(rot) @ points.transpose()
+    proj = K @ local_points
+    im_space = proj[:2] / proj[2]
+    accumulator = np.zeros((3, imsize[0], imsize[1]))
+    # TODO Vectorized
+    for i, (x, y) in enumerate(im_space.transpose()):
+        if proj[0, i] > 0 and check_points(x, y, IMSIZE):
+            accumulator[:, int(x), int(y)] = colors[i]
+    accumulator = np.transpose(accumulator, (2, 1, 0))
+    plt.imshow(accumulator)
+    plt.show()
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--pointcloud-file", default=POINTCLOUD_FILE, type=Path)
@@ -52,6 +78,9 @@ def main(pointcloud_file, trajectory_file):
 
     traj = pv.PolyData(locs)
     traj["index"] = np.arange(locs.shape[0])
+
+    for i in range(0, len(locs), 200):
+        project_points(xyzs, colors, locs[i], rots[..., i], K, IMSIZE)
 
     plotter = pv.Plotter()
     plotter.add_mesh(pc, scalars="class", rgb=True)
