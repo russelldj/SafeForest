d5592d1e89be4a045822cb1285af0abe37f9de80
David Russell <davidrussell327@gmail.com>
2022-01-24 19:40:35 -0500

    Add template

---
+++ dev/template.py
@@ -0,0 +1,20 @@
+import argparse
+
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument()
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """"""
+    pass
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main()
