35a072a6c0da3edf8728e003d4a254725a1a54f9
David Russell <davidrussell327@gmail.com>
2022-03-08 17:06:19 -0500

    Move confusion matrix code

---
+++ safeforest/model_evaluation/accuracy_computation.py
@@ -0,0 +1,14 @@
+import numpy as np
+from sklearn import metrics
+
+
+def accumulate_confusion_matrix(preds, gt, current_confusion, n_classes=7):
+    """Update a confusion matrix with predicted classes and gt class
+    TODO futher document
+    """
+    additional_confusion = metrics.confusion_matrix(
+        preds, gt, labels=np.arange(n_classes)
+    )
+    assert np.all(current_confusion.shape == additional_confusion.shape)
+    updated_confusion = current_confusion + additional_confusion
+    return updated_confusion
