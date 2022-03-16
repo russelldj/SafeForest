888f85843f3e0362bd043b0119540ba806befd50
David Russell <davidrussell327@gmail.com>
2022-03-15 22:38:00 -0400

    Initial notes on mmseg creator

---
+++ dev/dataset_creation/write_mmseg_configs.py
@@ -0,0 +1,9 @@
+import argparse
+from pathlib import Path
+
+# Takes a base dataset config
+# A base inherited network
+# A final derived network
+# A folder of folders to train on
+
+# Read in a set of file
